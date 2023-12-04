6e5431dfba46c967a30512378ebd6d26de1f5f05
Zach Fullmer <zachfullmer@Zachs-MacBook-Pro.local>
2023-12-04 16:24:36 -0700

    Fixed the JS to start with Alice and Bob from the start, then add a new freelancer every 3 seconds.

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Declare variables:
-const names = ["Bob", "Carol", "Derek", "Susan", "Josh", "Kristen", "Eric", "Anna", "Ava", "Jacob", "Harry"];
-const prices = [50, 70, 40, 65, 50, 100, 60, 45, 90, 80, 70];
+const names = ["Carol", "Derek", "Susan", "Josh", "Kristen", "Eric", "Anna", "Ava", "Jacob", "Harry"];
+const prices = [70, 50, 40, 65, 50, 100, 60, 45, 90, 80, 70];
 const occupations = ["Teacher", "Programmer", "Gardener", "Driver", "Writer"];
 const freelancers = [
   { name: "Alice", occupation: "Writer", price: 30 },
+  { name: "Bob", occupation: "Teacher", price: 50 },
 ];
 const maxFreelancers = 12;
 let index = 0;
 
 //Create an interval that will add a freelancer to the table each second:
-const addIntervalId = setInterval(addFreelancer, 1000);
+const addIntervalId = setInterval(addFreelancer, 3000);
 
 render(); // Render the initial state.
 
@@ -49,12 +50,20 @@ function render() {
 function addFreelancer() {
   //Declare variables used to obtain the next freelancer name, price, and occupation:
   const pr = prices[index];
-  const FLname = names[index];
+  const FLName = names[index];
   const occ = occupations[Math.floor(Math.random() * occupations.length)];
   index++;
+    /** Note: 
+     * I decided to display a freelancer in the order of the array list instead of randomizing it
+     * (which would be easy using the Math.floor(Math.random() * [array].length) method in the addFreelancer function). 
+     * I left the occupation as random to show that I know how to select a randomized element of an array list.
+     * I did it this way because it would more closely reflect a real program that would store
+     * a list of users and their price that I could then update later when each user changed their price
+     * or if they no longer wished it to be listed.
+    * */
 
   //Add freelancer to array list:
-  freelancers.push({ name: FLname, occupation: occ, price: pr });
+  freelancers.push({ name: FLName, occupation: occ, price: pr });
 
   //Update average starting price:
   const avg = averagePrice();
